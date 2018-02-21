069d4c5f5ade1c1ca5bd7c6fa476f0885aa75ffb
Alejandro Valor <36158323+AlexValor@users.noreply.github.com>
2018-02-21 09:31:09 +0100

    He modificado un ejercicio

--- XML-Python/xmlpython7.py
+++ XML-Python/xmlpython7.py
@@ -10,5 +10,3 @@ for provincia in provincias:
 		for localidades in provincia.findall("localidades/localidad"):
 			if localidades.attrib["c"]=="1":
 				print(localidad.text)
-			else:
-				print("No existe la provincia")	

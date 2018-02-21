ed21ce65a3c8e2e12ea5147ce7fc2d8e8d906d0d
AlexValor <valorbb@gmail.com>
2018-02-21 08:35:48 +0100

    He añadido un ejercicio

---
+++ XML-Python/xmlpython7.py
@@ -0,0 +1,14 @@
+from lxml import etree
+doc = etree.parse('provinciasypoblaciones.xml')
+
+provincias = doc.findall("provincia")
+provinciabuscar = input("Dime una provincia a buscar: ")
+	
+for provincia in provincias:
+	nombre = provincia.find("nombre")
+	if nombre.text == provinciabuscar:
+		for localidades in provincia.findall("localidades/localidad"):
+			if localidad.attrib["c"]==1:
+				print(localidad.text)
+			else:
+				print("No existe la provincia")	
